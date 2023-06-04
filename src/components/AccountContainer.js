f0c120e78d5d0d40f094ee4879600c5553c11b38
Moffat <moffat.muga@gmail.com>
2023-06-04 20:14:10 +0300

    First commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const fetchTransactions = async () => {
+    try {
+      const response = await fetch("http://localhost:8001/transactions");
+      const data = await response.json();
+      setTransactions(data);
+    } catch (error) {
+      console.log("Error fetching transactions:", error);
+    }
+  };
+
+  const handleAddTransaction = async (transaction) => {
+    try {
+      const response = await fetch("http://localhost:8001/transactions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(transaction),
+      });
+      const data = await response.json();
+      setTransactions([...transactions, data]);
+    } catch (error) {
+      console.log("Error adding transaction:", error);
+    }
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search onSearch={handleSearch} />
+      <AddTransactionForm onAddTransaction={handleAddTransaction} />
+      <TransactionsList transactions={filteredTransactions} />
     </div>
   );
 }
